23ec5311277515550288fa80663be542e13c12cd
prathamk1483 <133343956+prathamk1483@users.noreply.github.com>
2024-04-23 19:00:03 +0530

    Update DataVisualizaton.py

--- DataVisualizaton.py
+++ DataVisualizaton.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 iris= pd.read_csv("Iris.csv")
 
-# sns.pairplot(iris,hue='Species')
-# irisdf=iris.drop(['Id','Species'],axis=1)
-# iris.drop(["Id"],inplace=True)
-# plt.xlabel('Sepal Length')
-# plt.ylabel('Number of Values')
-# plt.title("Sepal Length Histogram")
-# plt.hist(irisdf['SepalLengthCm'],color='red', edgecolor='black')
-# plt.show()
+sns.pairplot(iris,hue='Species')
+irisdf=iris.drop(['Id','Species'],axis=1)
+iris.drop(["Id"],inplace=True)
+plt.xlabel('Sepal Length')
+plt.ylabel('Number of Values')
+plt.title("Sepal Length Histogram")
+plt.hist(irisdf['SepalLengthCm'],color='red', edgecolor='black')
+plt.show()
 
 
 plt.title("Sepal Length Boxplot")
@@ -21,30 +21,30 @@ sns.boxplot(data=iris['SepalLengthCm'])
 plt.boxplot(iris['SepalLengthCm'])
 plt.show()
 
-# sns.boxplot(irisdf)
-# plt.show()
+sns.boxplot(irisdf)
+plt.show()
 
 
 
-# plt.bar(irisdf['SepalLengthCm'].head(),irisdf["SepalWidthCm"].head(),width=1.1,edgecolor="black")
-# plt.show()
+plt.bar(irisdf['SepalLengthCm'].head(),irisdf["SepalWidthCm"].head(),width=1.1,edgecolor="black")
+plt.show()
 
 
-# plt.plot(irisdf["SepalLengthCm"])                                                               #didnt work
-# plt.show()
+plt.plot(irisdf["SepalLengthCm"])                                                               #didnt work
+plt.show()
 
-# sns.heatmap(irisdf['SepalLengthCm'].head())
-# plt.show()
+sns.heatmap(irisdf['SepalLengthCm'].head())
+plt.show()
 
-# sns.heatmap(iris.corr(),cmap='magma',annot=True)
-# plt.show()
+sns.heatmap(iris.corr(),cmap='magma',annot=True)
+plt.show()
 
-# plt.pie(irisdf["SepalLengthCm"].head(),labels=[0,1,2,3,4],explode=[0,0.1,0,0,0])
-# plt.show()
+plt.pie(irisdf["SepalLengthCm"].head(),labels=[0,1,2,3,4],explode=[0,0.1,0,0,0])
+plt.show()
 
 
-# sns.scatterplot(data=irisdf,x="SepalLengthCm",y="SepalWidthCm", size=20, sizes=(20, 2000))
-# plt.show()
+sns.scatterplot(data=irisdf,x="SepalLengthCm",y="SepalWidthCm", size=20, sizes=(20, 2000))
+plt.show()
 
 
 
